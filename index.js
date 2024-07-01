68ef89b2a825631a3a69f55cceb93187a2b15ee7
Patra Kirsani <patrakirsani.234ca035@nitk.edu.in>
2024-07-01 17:21:54 +0530

    second commit

--- index.js
+++ index.js
@@ -1,34 +1,62 @@
-const express = require("express");
+const express = require('express');
 const app = express();
 const port = 8080;
-const path = require("path");
+const path = require('path');
+const { v4: uuidv4} = require('uuid');
 
 app.use(express.urlencoded({extended: true}));
 
 app.set("view engine", "ejs");
-app.set("views", path.join(__dirname, "views"));
+app.set("view engine", path.join(__dirname, "views"));
 
-app.set(express.static(path.join(__dirname, "public")));
+app.use(express.static(path.join(__dirname, "public")));
 
 let posts = [
   {
-    username: "asherushare",
-    content: "I love coding!"
+    id: uuidv4(),
+    username: "admin",
+    content: "I love coding"
   },
   {
-    username: "shradhakhapra",
-    content: "Hard work is important to achieve success"
-  },
+    id: uuidv4(),
+    username: "admin2",
+    content: "I love dancing"
+  }, 
   {
-    username: "rahulkumar",
-    content: "I got selected for my 1st attempt"
-  }
+    id: uuidv4(),
+    username: "asheushare",
+    content: "I got selected for my 1st internship"
+  },
 ];
 
-app.get("/posts",(req, res) => {
-  res.render("index.ejs");
-} )
+app.get('/posts', (req, res) => {
+  res.render("index.ejs", {posts});
+})
+
+app.get('/posts/new', (req, res) => {
+  res.render('new.ejs');
+})
+
+app.get('/posts/:id', (req, res) => {
+  let {id} = req.params;
+  let post = posts.find((p) => id === p.id);
+  res.render('show.ejs', { post });
+});
+
+app.post('/posts', (req, res) => {
+  let {username, content} = req.body;
+  let id = uuidv4();
+  posts.push({id, username, content});
+  res.redirect('/posts');
+})
+
+app.patch('/posts/:id', (req, res) => {
+  let {id} = req.params;
+  consolge.log(id);
+  res.send('patch request working');
+})
 
 app.listen(port, () => {
   console.log("listening on port: 8080");
-})
+});
+
